33ebb0f5c6787f91b4abac2390f6b1c37677113c
glasfisch3000 <jakob@danckwerts.net>
2024-05-10 20:08:36 +0200

    updated TrendingItem structure

--- Sources/swift-piped/trending/TrendingItem.swift
+++ Sources/swift-piped/trending/TrendingItem.swift
@@ -2,12 +2,17 @@ import Foundation
 
 public struct TrendingItem {
     public var title: String
+    public var shortDescription: String
     public var thumbnail: URL
-    public var duration: Int
     public var views: Int
     
+    public var duration: Int
+    public var type: String
+    public var isShort: Bool
+    
+    public var uploaded: Int
     public var uploadedDate: String
-    public var uploader: String
+    public var uploaderName: String
     public var uploaderUrl: URL
     public var uploaderAvatar: URL
     public var uploaderVerified: Bool
